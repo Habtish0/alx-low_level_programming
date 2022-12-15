8867f81708528056800ee5fcff1a5f83ea54dfab
Habtish0 <habtu.ke@gmail.com>
2022-12-15 14:35:47 -0800

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is postive or nagative
+ * return: 0
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	postive_or_nagative(i)
+	
+	return 0;
+}
