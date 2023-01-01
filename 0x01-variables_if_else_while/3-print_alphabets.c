11f6b5dd77e955c513b8c02fad36d5abfac6830e
Habtish0 <habtu.ke@gmail.com>
2023-01-01 10:34:32 -0800

    print_alphabet in low case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,21 +3,19 @@
  * main - is the where the main function starts
  * Return: it returns void value
  */
-
 int main(void)
 {
-        char ch;
+	char ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                putchar(ch);
-        }
-        for (ch = 'A'; ch <= 'Z'; ch++)
-        {
-                putchar(ch);
-        }
-        
-        putchar('\n');
-        return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
 
