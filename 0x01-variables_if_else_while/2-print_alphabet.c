c1fa012018f89cb9af82469bc0c2824ed2211112
Habtish0 <habtu.ke@gmail.com>
2023-01-01 10:08:57 -0800

    print_alphabet in low case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - is the where the main function starts
+ * Return: it returns void value
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
