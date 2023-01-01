e148a73e259ba859c639cd20a4bd87ee8d9171d1
Habtish0 <habtu.ke@gmail.com>
2023-01-01 11:10:53 -0800

    Print all the letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - is the where the main function starts
+ * Return: it returns void value
+ */
+int main(void)
+{
+        char ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+                if (ch = 'e' || ch = 'q')
+                {
+                        continue;
+                }
+        putchar(ch);
+        }
+        putchar('\n');
+        return (0);
+}
