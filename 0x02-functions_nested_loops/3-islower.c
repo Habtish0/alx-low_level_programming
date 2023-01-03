30dea6fa468b6d9f468d0724a32a711075f8dba8
Habtish0 <habtu.ke@gmail.com>
2023-01-03 07:30:30 -0800

    task 04

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase characters
+ * Return: integer
+ */
+
+int _islower(int c)
+{
+	int c;
+	if (c >='a' && c <= 'z')
+	{
+		c = 1;
+	}
+	else
+	{
+		c = 0;
+	}
+	return (c);
+}
