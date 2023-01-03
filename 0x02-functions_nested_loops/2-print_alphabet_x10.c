70366bd39d8475c493a0b0793705f4a1bd96f8d3
Habtish0 <habtu.ke@gmail.com>
2023-01-03 07:14:46 -0800

    task 03

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
-	{		for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
-		 _putchar('\n');
+		_putchar('\n');
 	}
-	
 }
 
