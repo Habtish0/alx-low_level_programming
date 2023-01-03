1126b04a8611e2041c529c26b405ca2ae5e4ecd8
Habtish0 <habtu.ke@gmail.com>
2023-01-03 06:10:27 -0800

    task 02

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints alphabet in lowercase
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int char;
+
+	for (char = 'a'; char <= 'z'; char++)
+	{
+		-putchar(char);
+	}
+	return (0);
+}
